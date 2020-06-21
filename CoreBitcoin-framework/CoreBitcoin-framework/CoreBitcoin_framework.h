bf0d0ccf03d80812834a374e521febc1f09c9db0
王欣 <wangxin102@58.com>
2020-06-21 13:55:59 +0800

    第一次提交

---
+++ CoreBitcoin-framework/CoreBitcoin-framework/CoreBitcoin_framework.h
@@ -0,0 +1,68 @@
+//
+//  CoreBitcoin_framework.h
+//  CoreBitcoin-framework
+//
+//  Created by 王欣 on 2020/6/21.
+//  Copyright © 2020 王欣. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreBitcoin_framework.
+FOUNDATION_EXPORT double CoreBitcoin_frameworkVersionNumber;
+
+//! Project version string for CoreBitcoin_framework.
+FOUNDATION_EXPORT const unsigned char CoreBitcoin_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreBitcoin_framework/PublicHeader.h>
+#import "BTCPaymentMethodDetails.h"
+#import "BTCProcessor.h"
+#import "BTCCurvePoint.h"
+#import "BTCData.h"
+#import "BTCMerkleTree.h"
+#import "BTCOutpoint.h"
+#import "BTCProtocolBuffers.h"
+#import "BTC256.h"
+#import "BTCAddress.h"
+#import "BTCPaymentMethod.h"
+#import "BTCHashID.h"
+#import "BTCCurrencyConverter.h"
+#import "BTCScript.h"
+#import "BTCPaymentRequest.h"
+#import "BTCTransactionOutput.h"
+#import "BTCAddressSubclass.h"
+#import "BTCKey.h"
+#import "BTCEncryptedMessage.h"
+#import "BTCBase58.h"
+#import "BTCPaymentMethodRequest.h"
+#import "BTCPaymentProtocol.h"
+#import "BTCErrors.h"
+#import "BTCBlindSignature.h"
+#import "BTCQRCode.h"
+#import "BTCAssetID.h"
+#import "BTCTransactionBuilder.h"
+#import "BTCAssetType.h"
+#import "BTCKeychain.h"
+#import "BTCEncryptedBackup.h"
+#import "BTCBlock.h"
+#import "BTCBlockHeader.h"
+#import "BTCNumberFormatter.h"
+#import "BTCBlockchainInfo.h"
+#import "BTCBigNumber.h"
+#import "BTCTransaction.h"
+#import "BTCUnitsAndLimits.h"
+#import "BTCFancyEncryptedMessage.h"
+#import "BTCOpcode.h"
+#import "BTCPriceSource.h"
+#import "BTCSecretSharing.h"
+#import "BTCSignatureHashType.h"
+#import "BTCMnemonic.h"
+#import "BTCProtocolSerialization.h"
+#import "BTCNetwork.h"
+#import "BTCScriptMachine.h"
+#import "BTCTransactionInput.h"
+#import "BTCAssetAddress.h"
+#import "BTCBitcoinURL.h"
+#import "BTCChainCom.h"
+
+
---
+++ CoreBitcoin.podspec
@@ -0,0 +1,18 @@
+
+Pod::Spec.new do |spec|
+
+spec.name         = 'CoreBitcoin-framework'
+spec.version      = '1.0.8'
+spec.summary      = 'CoreBitcoin 封装'
+spec.license      = 'MIT'
+spec.homepage     = 'https://github.com/SmartPear/CoreBitcoin'
+spec.author       = {"王欣" => "=573385822@qq.com" }
+spec.platform     = :ios, '9.0'
+spec.source       = {:git => "https://github.com/SmartPear/CoreBitcoin.git", :tag => spec.version}
+spec.frameworks = 'Foundation'
+spec.source_files  = 'CoreBitcoin-framework/CoreBitcoin-framework/*.{h,m}','CoreBitcoin-framework/CoreBitcoin-framework/category/*.{h,m}'
+spec.swift_version = '5.0'
+spec.dependency "OpenSSL-for-iOS" , '~> 1.0.2.d.1'
+spec.dependency "secp256k1.c" ,'0.1.2'
+
+end
